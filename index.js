8bc900c60288b6e24c65901510897b835a870e61
yipdamon <yipdamon@hotmail.com>
2021-10-05 22:54:34 -0400

    added api routes

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get('/', (req, res) => {
     res.send("Test express");
 });
 
+// api routes
+app.get('/api/penguins', (req, res) => {
+
+    // return dummy data
+    const cats = ["fact1", "fact2", "fact3"];
+
+    // sending a response should be in JSON
+    res.send(JSON.stringify(cats));
+});
+
+app.get('/api/bears', (req, res) => {
+
+    const cats = ["fact11", "fact12", "fact13"];
+    res.send(JSON.stringify(cats));
+});
+
 // run server on port
 app.listen(port, () => {
     console.log("Listening on port", port);
